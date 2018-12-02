285aa490fda50c2c08141394b36b6f9eb9250b59
Clark <clarkdevlin@gmail.com>
2018-12-02 23:17:41 +0800

    add gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,27 @@
 const gulp = require('gulp'),
-watch =require('gulp-watch')
-
-
-gulp.task('html', function(){
-    console.log('HTML task performed')
-})
-
-gulp.task('styles', function(){
-    console.log('CSS task performed')
-})
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested')
 
 gulp.task('watch', function(){
     watch('./app/index.html', function(){
-        gulp.start('html')
+        htmlProcess()
     })
 
     watch('./app/assets/styles/**/*.css', function(){
-        gulp.start('styles')
+        stylesProcess()
     })
 })
+
+function htmlProcess(){
+    console.log('HTML task performed')
+}
+
+function stylesProcess(){
+    console.log("Processing CSS")
+    return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))        
+    .pipe(gulp.dest('./app/temp/styles'))
+}
