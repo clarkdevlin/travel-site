97e9193fbad7f0ea9db7ad2590f1db775db1ea0b
Clark <clarkdevlin@gmail.com>
2018-12-03 18:02:30 +0800

    Lesson 13

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested')
+nested = require('postcss-nested'),
+cssImport = require('postcss-import')
 
 gulp.task('watch', function(){
     watch('./app/index.html', function(){
@@ -22,6 +23,6 @@ function htmlProcess(){
 function stylesProcess(){
     console.log("Processing CSS")
     return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssvars, nested, autoprefixer]))        
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))        
     .pipe(gulp.dest('./app/temp/styles'))
 }
