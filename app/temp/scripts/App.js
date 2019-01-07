0f3de76da77e02b3e6f2ff5a42f7ce1690e22536
Clark <clarkdevlin@gmail.com>
2019-01-07 14:32:49 +0800

    Complete the modal overlay

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -95,6 +95,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _modules_StickyHeader__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5);
 /* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(2);
 /* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _modules_Modal__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(7);
+
+
 
 
 
@@ -103,6 +106,7 @@ var mobileMenu = new _modules_MobileMenu__WEBPACK_IMPORTED_MODULE_0__["default"]
 new _modules_RevealOnScroll__WEBPACK_IMPORTED_MODULE_1__["default"](jquery__WEBPACK_IMPORTED_MODULE_3___default()('.feature-item'), "85%");
 new _modules_RevealOnScroll__WEBPACK_IMPORTED_MODULE_1__["default"](jquery__WEBPACK_IMPORTED_MODULE_3___default()('.testimonial'), "60%");
 var stickyHeader = new _modules_StickyHeader__WEBPACK_IMPORTED_MODULE_2__["default"]();
+var modal = new _modules_Modal__WEBPACK_IMPORTED_MODULE_4__["default"]();
 
 /***/ }),
 /* 1 */
@@ -11799,6 +11803,70 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
 }));
 
 
+
+/***/ }),
+/* 7 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
+/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_0__);
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+
+
+var Modal =
+/*#__PURE__*/
+function () {
+  function Modal() {
+    _classCallCheck(this, Modal);
+
+    this.openModalButton = jquery__WEBPACK_IMPORTED_MODULE_0___default()('.open-modal');
+    this.modal = jquery__WEBPACK_IMPORTED_MODULE_0___default()('.modal');
+    this.closeModalButton = jquery__WEBPACK_IMPORTED_MODULE_0___default()('.modal__close');
+    this.events();
+  }
+
+  _createClass(Modal, [{
+    key: "events",
+    value: function events() {
+      //clicking the open modal button
+      this.openModalButton.click(this.openModal.bind(this)); //clicking the X close modal button
+
+      this.closeModalButton.click(this.closeModal.bind(this)); //pushes any key
+
+      jquery__WEBPACK_IMPORTED_MODULE_0___default()(document).keyup(this.keypressHandler.bind(this));
+    }
+  }, {
+    key: "keypressHandler",
+    value: function keypressHandler(e) {
+      if (e.keyCode == 27) {
+        this.closeModal();
+      }
+    }
+  }, {
+    key: "openModal",
+    value: function openModal() {
+      this.modal.addClass('modal--is-visible');
+      return false;
+    }
+  }, {
+    key: "closeModal",
+    value: function closeModal() {
+      this.modal.removeClass('modal--is-visible');
+      return false;
+    }
+  }]);
+
+  return Modal;
+}();
+
+/* harmony default export */ __webpack_exports__["default"] = (Modal);
 
 /***/ })
 /******/ ]);
